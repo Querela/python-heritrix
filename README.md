9e87727ff536946aa7b3ee98d4006eee746d4060
Querela <ek.92@arcor.de>
2019-04-16 17:03:55 +0200

    Update api (rewrite in my style ...) (#1)
    * Update api (rewrite in my style ...)
    
    - complete rewrite (after Heritrix Doc, rename params)
    - refactor request methods
    - add exceptions instead of `None` returns
    - set required params not as keyword arguments/without default
    
    * Update __init__.py imports
    
    * Add lxml requirement in setup.py
    
    * Add lxml to requirements.txt
    
    * Update README.md

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ python-heritrix
 A simple wrapper around the Heritrix 3.x API:
 
   https://webarchive.jira.com/wiki/display/Heritrix/Heritrix+3.x+API+Guide
+  
+or newer (old is redirect):
+
+  https://heritrix.readthedocs.io/en/latest/api.html
 
 Developed in April 2012 against Heritrix 3.1.0 at GWU Libraries in
 Washington, DC, USA.
@@ -15,3 +19,17 @@ known issues
 Ubuntu versions later than 10.04 LTS ship with an openssl version that
 leads to the SSL error noted in the API guide (link above).  This does
 not occur with 10.04 LTS.
+
+Disable `InsecureRequestWarning` warnings for un-verified https requests:
+
+```python
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+```
+
+todo
+----
+
+Add some more scripts to API?:
+
+  https://github.com/internetarchive/heritrix3/wiki/Heritrix3%20Useful%20Scripts#Heritrix3UsefulScripts-dumpsurts
--- heritrix/api.py
+++ heritrix/api.py
@@ -1,107 +1,279 @@
-import sys
 import logging
+import os
+
 import requests
 from lxml import etree
-import xml.etree.ElementTree as ET
 from requests.auth import HTTPDigestAuth
 
 requests_log = logging.getLogger( "requests" )
 requests_log.setLevel( logging.WARNING )
 
-class API(object):
-    def __init__(self, host="https://localhost:8443/engine",
-        user="admin", passwd="", verbose=False, verify=False):
+# ---------------------------------------------------------------------------
+
+
+class HeritrixAPIError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super(HeritrixAPIError, self).__init__(message)
+
+    def __str__(self):
+        return "HeritrixAPIError: {}".format(self.message)
+
+
+class HeritrixAPI(object):
+    def __init__(
+        self,
+        host="https://localhost:8443/engine",
+        user="admin",
+        passwd="",
+        verbose=False,
+        verify=False,
+        headers=None,
+    ):
         self.host = host
         self.user = user
         self.passwd = passwd
-        self.config = {}
-        if verbose:
-            self.config["verbose"] = sys.stderr
+        self.verbose = verbose
         self.verify = verify
 
-    def _get(self, url=""):
-        headers = {"Accept": "application/xml"}
-        r = requests.get(url, auth=HTTPDigestAuth(self.user, self.passwd), headers=headers, verify=self.verify)
-        return r
+        # strip trailing slashes
+        if self.host:
+            self.host = self.host.rstrip("/")
 
-    def _post(self, action="", url="", data={}):
-        if action == "":
-            return None
+        self.headers = {"Accept": "application/xml"}
+        if isinstance(headers, dict):
+            self.headers.update(headers)
+
+    def _post(self, url=None, data=None, headers=None):
         if not url:
             url = self.host
+
+        headers_copy = dict(self.headers)
+        if headers is not None:
+            headers_copy.update(headers)
+
+        if data is None:
+            data = dict()
+
+        resp = requests.post(
+            url,
+            auth=HTTPDigestAuth(self.user, self.passwd),
+            data=data,
+            headers=headers_copy,
+            verify=self.verify,
+        )
+        return resp
+
+    def _get(self, url=None, headers=None, api_headers=True):
+        if not url:
+            url = self.host
+
+        headers_copy = dict()
+        if api_headers:
+            headers_copy.update(self.headers)
+        if headers is not None:
+            headers_copy.update(headers)
+
+        resp = requests.get(
+            url,
+            auth=HTTPDigestAuth(self.user, self.passwd),
+            headers=headers_copy,
+            verify=self.verify,
+        )
+        return resp
+
+    def _post_action(self, action, url=None, data=None, headers=None):
+        if action is None or action == "":
+            raise HeritrixAPIError("Missing action.")
+
+        if data is None:
+            data = dict()
+
         data["action"] = action
-        headers = {"Accept": "application/xml"}
-        r = requests.post(url, auth=HTTPDigestAuth(self.user, self.passwd),
-            data=data, headers=headers, verify=self.verify)
-        return r
 
-    def add(self, addpath=""):
-        action = "add"
-        if addpath == "":
-            return None
-        return self._post(action, data={"addpath": addpath})
+        return self._post(url=url, data=data, headers=headers)
 
-    def create(self, createpath=""):
-        action = "create"
-        if createpath == "":
-            return None
-        return self._post(action, data={"createpath": createpath})
+    def _job_action(self, action, job_name, data=None):
+        if job_name is None or job_name == "":
+            raise HeritrixAPIError("Missing job name.")
+
+        url = "{host}/job/{job}".format(host=self.host, job=job_name)
+
+        return self._post_action(action, url, data=data)
+
+    # --------------------------------
+
+    def create(self, job_name):
+        if job_name is None or job_name == "":
+            raise HeritrixAPIError("Missing job name.")
+
+        return self._post_action("create", data={"createpath": job_name})
+
+    def add(self, job_dir):
+        if job_dir is None or job_dir == "":
+            raise HeritrixAPIError("Missing job directory.")
+        # TODO: check that a cxml file is in the directory?
+
+        return self._post_action("add", data={"addpath": job_dir})
+
+    # --------------------------------
+
+    def build(self, job_name):
+        return self._job_action("build", job_name)
+
+    def launch(self, job_name, checkpoint=None):
+        data = None
+        if checkpoint is not None:
+            data = {"checkpoint": checkpoint}
+
+        return self._job_action("build", job_name, data=data)
+
+    def pause(self, job_name):
+        return self._job_action("build", job_name)
+
+    def unpause(self, job_name):
+        return self._job_action("unpause", job_name)
+
+    def terminate(self, job_name):
+        return self._job_action("terminate", job_name)
+
+    def teardown(self, job_name):
+        return self._job_action("teardown", job_name)
+
+    def checkpoint(self, job_name):
+        return self._job_action("checkpoint", job_name)
+
+    # --------------------------------
 
     def rescan(self):
-        action = "rescan"
-        return self._post(action)
+        return self._post_action("rescan")
 
-    def _job_action(self, action="", job=""):
-        if action == "" and job == "":
-            return self._get(url=self.host)
+    def copy(self, job_name, new_job_name, as_profile=False):
+        if new_job_name is None or new_job_name == "":
+            raise HeritrixAPIError("new_job_name must not be empty!")
+
+        data = dict()
+        data["copyTo"] = new_job_name
+        if as_profile:
+            data["as_profile"] = "on"
+
+        return self._job_action("copy", job_name, data=data)
+
+    # --------------------------------
+
+    def execute_script(self, job_name, script, engine="beanshell"):
+        if job_name is None or job_name == "":
+            raise HeritrixAPIError("Missing job name.")
+        if script is None or script == "":
+            raise HeritrixAPIError("Missing script?")
+        if engine not in ("beanshell", "js", "groovy", "AppleScriptEngine"):
+            raise HeritrixAPIError("Invalid script engine param: {}".format(engine))
+
+        data = dict()
+        data["engine"] = "js"
+        data["script"] = script
+
+        url = "{host}/job/{job}/script".format(host=self.host, job=job_name)
+
+        return self._post(url=url, data=data)
+
+    # --------------------------------
+
+    def send_config(self, job_name, cxml_filepath):
+        if job_name is None or job_name == "":
+            raise HeritrixAPIError("Missing job name.")
+        if cxml_filepath is None or cxml_filepath == "":
+            raise HeritrixAPIError("Missing cxml filepath name.")
+        if not os.path.exists(cxml_filepath) or not os.path.isfile(cxml_filepath):
+            raise HeritrixAPIError(
+                "CXML file does not exist!, {}".format(cxml_filepath)
+            )
+
+        url = "{host}/job/{job}/jobdir/crawler-beans.cxml".format(
+            host=self.host, job=job_name
+        )
+
+        with open(cxml_filepath, "rb") as fdat:
+            resp = requests.put(
+                url,
+                auth=HTTPDigestAuth(self.user, self.passwd),
+                data=fdat,
+                headers=self.headers,
+                verify=self.verify,
+            )
+        return resp.ok
+
+    # --------------------------------
+
+    def list_jobs(self, status=None, unbuilt=False):
+        resp = self._get()
+        xml_doc = etree.fromstring(resp.text)
+
+        if unbuilt:
+            # if unbuilt, then search for those only
+            jobs = xml_doc.xpath("//jobs/value[./statusDescription = 'Unbuilt']")
+        elif status is not None:
+            # then search for crawlControllerState
+            jobs = xml_doc.xpath(
+                "//jobs/value[./crawlControllerState = '{}']".format(status)
+            )
         else:
-            url = "%s/job/%s" % (self.host, job)
-            if action == "":
-                return self._get(url=url)
-            else:
-                return self._post(action=action, url=url)
+            # else all
+            jobs = xml_doc.xpath("//jobs/value")
 
-    def build(self, job=""):
-        return self._job_action(action="build", job=job)
+        job_names = [job.find("shortName").text for job in jobs]
+        return job_names
 
-    def launch(self, job=""):
-        return self._job_action(action="launch", job=job)
-        
-    def pause(self, job=""):
-        return self._job_action(action="pause", job=job)
+    def get_launchid(self, job_name):
+        script = "rawOut.println( appCtx.getCurrentLaunchId() );"
+        resp = self.execute_script(job_name, script=script, engine="beanshell")
+        if not resp.ok:
+            if resp.status_code == 500:
+                # most probably not application context / unbuilt job
+                return None
 
-    def unpause(self, job=""):
-        return self._job_action(action="unpause", job=job)
+            raise HeritrixAPIError(
+                "No launchid found: {} - {}".format(resp.status_code, resp.reason)
+            )
 
-    def terminate(self, job=""):
-        return self._job_action(action="terminate", job=job)
+        tree = etree.fromstring(resp.text)
+        return tree.find("rawOutput").text.strip()
 
-    def teardown(self, job=""):
-        return self._job_action(action="teardown", job=job)
+    def crawl_report(self, job_name, launch_id=None):
+        if launch_id is None:
+            try:
+                # if no launchid - try to get with "latest"
+                url = "{host}/job/{job}/jobdir/latest/reports/crawl-report.txt".format(
+                    host=self.host, job=job_name
+                )
 
-    def checkpoint(self, job=""):
-        return self._job_action(action="checkpoint", job=job)
+                resp = self._get(url=url, api_headers=False)
+                return resp.text
+            except Exception as ex:
+                # if that fails, try to query the launch_id and try again
+                launch_id = self.get_launchid(job_name)
 
-    def copy(self, copyTo="", asProfile=False):
-        if copyTo == "":
-            return None
-        url = "%s/job/%s" % (self.host, job)
-        data = {"copyTo": copyTo}
-        if asProfile:
-            data["asProfile"] = "on"
-        else:
-            data["asProfile"] = "off"
-        headers = {"Accept": "application/xml"}
-        r = requests.post(url=url, auth=(self.user, self.passwd),
-            data=data, headers=headers, verify=self.verify)
-        return r
+                if launch_id is None:
+                    # unbuilt job?
+                    # either it got anything with latest or there simply was not yet a crawl
+                    raise HeritrixAPIError(
+                        "Unbuilt Job %s, check if has ever crawled?", job_name
+                    ) from ex
 
-    def submit(self, job="", urls=[], config={}):
-        # NOTE: is a PUT.
-        # FIXME: hmm, what would be useful here?
-        #r = requests.post(url=url, auth=(self.user, self.passwd),
-        #    verify=self.verify)
-        return
+                return self.crawl_report(job_name, launch_id=launch_id)
+
+        # ----------------------------
+
+        url = "{host}/job/{job}/jobdir/{id}/reports/crawl-report.txt".format(
+            host=self.host, job=job_name, id=launch_id
+        )
+
+        resp = self._get(url=url, api_headers=False)
+        return resp.text
+
+
+    # ---------------------------------------------------------------------------
+    # old    
 
     def status(self, job=""):
         xml = ET.fromstring( self._job_action(action="",job=job).text )
@@ -110,25 +282,6 @@ class API(object):
             return ""
         else:
             return status.text
-
-    def listjobs(self, status=None):
-        xml = etree.fromstring(self._job_action(action="",job="").content)
-        if status == None:
-            return [job.find("shortName").text for job in xml.xpath("//jobs/value")]
-        else:
-            return [job.find("shortName").text for job in xml.xpath("//jobs/value[./crawlControllerState = '%s']" % status)]
-
-    def execute(self, engine="beanshell", script="", job=""):
-        if script == "":
-            return None
-        url = "%s/job/%s/script" % (self.host, job)
-        return self._post(url=url, action="execute", data={"engine": engine, "script": script})
-
-    def launchid(self, job=""):
-        script = "rawOut.println( appCtx.getCurrentLaunchId() );"
-        xml = self.execute( engine="beanshell", script=script, job=job )
-        tree = ET.fromstring( xml.content )
-        return tree.find( "rawOutput" ).text.strip()
 
     def seeds( self, job ):
         url = "%s/job/%s/jobdir/latest/seeds.txt" % ( self.host, job )
@@ -141,7 +294,7 @@ class API(object):
 
     def empty_frontier( self, job ):
         script = "count = job.crawlController.frontier.deleteURIs( \".*\", \"^.*\" )\nrawOut.println count"
-        xml = self.execute( engine="groovy", script=script, job=job )
-        tree = ET.fromstring( xml.content )
+        xml = self.execute_script( job, script, engine="groovy" )
+        tree = etree.fromstring( xml.content )
         return tree.find( "rawOutput" ).text.strip()
 
